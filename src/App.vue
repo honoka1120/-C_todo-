89ef7c27533e4cfd747778498caa637ae5155f64
honoka1120 <honoka.mashiko1120@gmail.com>
2021-08-06 03:26:29 +0900

    トップ画面変更

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Top</router-link> |
       <router-link to="/about">About</router-link>|
       <router-link to="/todolist">todolist</router-link>|
       <router-link to="/addtodo">Addtodo</router-link>
